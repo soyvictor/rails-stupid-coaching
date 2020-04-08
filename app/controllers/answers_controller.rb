f4c17832e19ee6618e83d1c7742ee107aded9c05
Victor Chiang <vgchiang@gmail.com>
2020-04-08 17:02:59 -0500

    commited

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,13 @@
+class AnswersController < ApplicationController
+  def answer
+    @question = params[:question]
+
+    if params[:question] == 'I am going to work'
+      @answer = 'Great!'
+    elsif params[:question].end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
